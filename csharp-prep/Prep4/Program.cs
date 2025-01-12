01b19a7b1402f00b44418bc2e8adc7fa01bd75ef
Garrison Hammer <ham20051@byui.edu>
2025-01-12 10:53:02 -0700

    created the number appending program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        //set up the loops
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to end) ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            //adds numbers to the list if not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"The total is: {total}");
+
+        //sets up the variables as floats 
+        //finds the average
+        float average = ((float)total)/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //find the max values
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                //compares the current number to the next
+                //assigns the number to max if larger
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
